7fe6a457ac2068c38d690ae9e4d4d80db84c1a3e
b2211850_cist <b2211850@photon.chitose.ac.jp>
2022-11-01 21:24:34 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class kadai2 {
+    public static void main(String[] args) {
+        int studentNumber = 2211850;
+        System.out.println("B" + studentNumber + " Syota Nisino");
+    }
+}
