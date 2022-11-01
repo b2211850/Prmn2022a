b934ef7c6d972d7d7f196eb91ae283b60ec1f46d
b2211850_cist <b2211850@photon.chitose.ac.jp>
2022-11-01 21:44:07 +0900

    課題1-4

---
+++ prmn2022a/src/lecture01/kadai4.java
@@ -0,0 +1,15 @@
+package lecture01;
+
+public class kadai4 {
+    public static void main(String[] args) {
+        int array[] = new int[100];
+        int sum = 0;
+        for(int i = 0 ; i < array.length ; i++ ){
+            array[i] = i + 1;
+            if(i % 2 == 0){
+                sum =sum + array[i];
+            }
+        }
+        System.out.println("配列番号が偶数の値の総和　= " + sum );
+    }
+}
