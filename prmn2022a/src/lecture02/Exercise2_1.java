5e35c118f8d192d5c2161b96eb4efaa565fdf72a
b2211850_cist <b2211850@photon.chitose.ac.jp>
2022-11-04 20:44:02 +0900

    課題2-1

---
+++ prmn2022a/src/lecture02/Exercise2_1.java
@@ -0,0 +1,12 @@
+package lecture02;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {;
+
+        Human h1 = new Human("田中",17);
+        h1.print();
+        Human h2 = new Human("渡辺",21);
+        h2.print();
+
+    }
+}
