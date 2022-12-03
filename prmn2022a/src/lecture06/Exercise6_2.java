62936f87175884fa151d0cb6e35c25559c07f7fc
b2211850_cist <b2211850@photon.chitose.ac.jp>
2022-12-03 22:30:03 +0900

    課題6-2

---
+++ prmn2022a/src/lecture06/Exercise6_2.java
@@ -0,0 +1,35 @@
+package lecture06;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise6_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        ArrayList<Integer> dise = new ArrayList<>();
+
+        for(int i=0 ; i<5 ; i++ ) {
+            dise.add(random.nextInt(6) + 1);
+        }
+        System.out.println("さいころを5つ振りました");
+
+        System.out.println("何番目のさいころの値を確認しますか？");
+        int k = input.nextInt();
+
+        try{
+            int a = dise.get(k);
+            System.out.println(k+"番目のさいころの目は"+a+"です");
+        }catch (Exception e){
+            System.out.println("ArrayListの範囲外アクセスを確認しました");
+            System.out.println("プログラムを終了します");
+            System.exit(0);
+
+        }
+
+    }
+
+
+
+}
